7112df2e1438ea288f653fcb0139a6ffdbfb3f03
CarlosEBN <cebarco@estudiante.uniajc.edu.co>
2025-05-08 00:28:03 -0500

    este si

--- src/main/java/com/inventario/demo/controller/MovimientoInventarioController.java
+++ src/main/java/com/inventario/demo/controller/MovimientoInventarioController.java
@@ -1,4 +1,8 @@
 package com.inventario.demo.controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import com.inventario.demo.model.MovimientoInventario;
 import com.inventario.demo.service.MovimientoInventarioService;
@@ -43,7 +47,7 @@ public class MovimientoInventarioController {
         return null;
     }
 
-    @DeleteMapping(oath="borrarmovimiento/{id}")
+    @DeleteMapping(path ="borrarmovimiento/{id}")
     public void deleteMovimiento(@PathVariable Long id) {
         movimientoService.deleteById(id);
     }
--- src/main/java/com/inventario/demo/controller/UsuarioController.java
+++ src/main/java/com/inventario/demo/controller/UsuarioController.java
@@ -5,6 +5,8 @@ import com.inventario.demo.service.UsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
 
 import java.util.List;
 
